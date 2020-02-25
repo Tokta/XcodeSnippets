28197b4caad7d6fda54067d6ac8c066fec95a1d7
Alessio Sardella <alessio@lepaya.com>
2020-02-25 10:17:42 +0100

    auto commit, date: 25-02-2020-10:17:42

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 echo "Change directory to snippets repository"
-cd /Users/alessio/Documents/GitHub/XCodeSnippets
+cd /Users/alessiosardella/Documents/GitHub/XCodeSnippets
 echo "Monitoring snippets changes..."
-fswatch -0 /Users/alessio/Documents/GitHub/XCodeSnippets/CodeSnippets | xargs -0 -n1 -I{} zsh /Users/alessio/Documents/GitHub/XCodeSnippets/auto_commit_push.sh
+fswatch -0 /Users/alessiosardella/Documents/GitHub/XCodeSnippets/CodeSnippets | xargs -0 -n1 -I{} zsh /Users/alessiosardella/Documents/GitHub/XCodeSnippets/auto_commit_push.sh
